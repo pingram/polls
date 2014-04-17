321f3380637bb45b6a495064bffe0f0113e32d60
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-17 11:22:10 -0700

    updte models

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,8 @@
 class Poll < ActiveRecord::Base
-
+  belongs_to(
+    :author,
+    class_name: "User",
+    foreign_key: :author_id,
+    primary_key: :id
+  )
 end
---
+++ app/models/response.rb
@@ -0,0 +1,17 @@
+class Response < ActiveRecord::Base
+  belongs_to(
+    :respondent,
+    class_name: "User",
+    foreign_key: :user_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :answer_choice,
+    class_name: "AnswerChoice",
+    foreign_key: :answer_choice_id,
+    primary_key: :id
+  )
+
+
+end
