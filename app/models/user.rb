cef27854b923ac248895452045891b74131ef824
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-17 15:44:53 -0700

    TWO QUERIESgit add -A! completed_polls

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,43 +21,54 @@ class User < ActiveRecord::Base
 
     # all the polls that we've answered a question in, plus the questions we've answered!
 
-    query = <<-SQL
-    SELECT
-    polls.*, COUNT(*) AS q_count
-    FROM
-    polls
-    JOIN
-    questions
-    ON
-    polls.id = questions.poll_id
-    JOIN
-    answer_choices AS ac
-    ON
-    ac.question_id = questions.id
-    JOIN
-    responses
-    ON
-    responses.answer_choice_id = ac.id
-    WHERE
-    responses.user_id = #{self.id}
-    GROUP BY
-    polls.id
+    answered_query = <<-SQL
+      SELECT
+        polls.*, COUNT(*) AS q_count
+      FROM
+        polls
+      JOIN
+        questions
+      ON
+        polls.id = questions.poll_id
+      JOIN
+        answer_choices AS ac
+      ON
+        ac.question_id = questions.id
+      JOIN
+        responses
+      ON
+        responses.answer_choice_id = ac.id
+      WHERE
+        responses.user_id = #{self.id}
+      GROUP BY
+        polls.id
     SQL
 
-    answered_polls = Poll.find_by_sql(query)
+    answered_polls = Poll.find_by_sql(answered_query)
 
     # all the polls with their question count!
 
-    poll_questions = Poll.all.includes(:questions)
+    all_query = <<-SQL
+      SELECT
+        polls.*, COUNT(*) AS q_count
+      FROM
+        polls
+      JOIN
+        questions
+      ON
+        polls.id = questions.poll_id
+      GROUP BY
+        polls.id
+    SQL
+
+    poll_questions = Poll.find_by_sql(all_query)
 
     poll_questions.each.with_object([]) do |pq, completed|
-      ans_pol = answered_polls.select {|poll| poll.id == pq.id}.first
-      if !ans_pol.nil? && pq.questions.length == ans_pol.q_count
+      ans_pol = answered_polls.select { |poll| poll.id == pq.id }.first
+      if !ans_pol.nil? && pq.q_count == ans_pol.q_count
         completed << pq
       end
     end
-
-    # for each poll, we want to get the questions
 
   end
 
